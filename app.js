3ae382a111c3030490e695afeb350c07e5d59815
Roshan Adhikari <nahsorad@gmail.com>
2019-03-12 17:20:32 +0545

    Log usernames to the server

---
+++ app.js
@@ -0,0 +1,35 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+let users = [];
+io.on('connection', (socket) => {
+    console.log('a user has connected');
+
+    socket.on('setUsername', (username) =>{
+        console.log('USER/login-query - ' + username);
+        console.log(users.indexOf(username));
+        if(users.indexOf(username) <= -1){
+            users.push(username);
+            socket.emit('userSet', {username: username});
+            console.log('USER - ' + username + ' logged in!');
+        }
+        else{
+            socket.emit('usernameExists', username);
+            console.log('USER - ' + username + ' already exists!');
+        }
+    });
+
+    socket.on('disconnect', () => {
+        console.log('a user has disconnected');
+    });
+});
+
+http.listen(PORT, () => {
+    console.log(`Server is now listening on port ${PORT}`);
+});
